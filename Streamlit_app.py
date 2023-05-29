75e858e49b710bb7c182d9c349f6c0e743a9bcdb
Rajeshkatla22 <134928736+Rajeshkatla22@users.noreply.github.com>
2023-05-29 19:16:03 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Moms New Healty Diner')
 streamlit.header('BreakFast Menu')
-streamlit.text('🥣 omega 3 & Blureberry Oatmeal')
+streamlit.text('🥣 omega 4 & Blureberry Oatmeal')
 streamlit.text('🥗 kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
