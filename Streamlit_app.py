d76f4245d457da9a2d6983f27e58b6c32e84bad0
Rajeshkatla22 <134928736+Rajeshkatla22@users.noreply.github.com>
2023-06-09 14:53:25 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -32,10 +32,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.Header("te Fruit load list contains:")
-streamlit.dataframe(my_data_row)
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
