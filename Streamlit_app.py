dec146b12806e47145a74d30ecea0291e1fc4feb
Rajeshkatla22 <134928736+Rajeshkatla22@users.noreply.github.com>
2023-06-09 18:05:45 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -22,21 +22,19 @@ streamlit.dataframe(fruits_to_display)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information.")
   else:
-# take the version of the resopnce and normalise it
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output is the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 excepy UELError as e:
   streamlit.error()
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
